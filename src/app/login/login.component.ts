cc2fe9ad68e0d7c5c60aba29db20c910e3fd19b2
NAGARJUN <dasnagarjun1994@gmail.com>
2022-03-24 13:51:52 +0530

    v2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+loginForm!: FormBuilder;
+  constructor(private FormBuilder:FormBuilder) { }
 
   ngOnInit(): void {
   }
